dcae25640fff72c15c17b2b2f104f76c70ad94a9
Carlo Panzi <carlo.panzi@gmail.com>
2016-08-02 11:30:59 +0200

    template substitution

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs-promise');
 const options = require('commander');
+
 const epub = require('./lib/EpubTemplate');
 
 
@@ -9,18 +10,31 @@ options
 .usage('[options] <file.md>')
 .parse(process.argv);
 
+
 process.on('exit', () => {
-	epub.clear();
+	//epub.clear();
 });
 
 
 // Set to avoid duplicates
 let files = new Set(options.args.filter((file) => file.match(/.md$/i)));
 
+let tempData = {
+	title: 'Le avventure di carlo',
+	creator: 'Carlo'
+}
 
 for (let file of files) {
 
 	console.log(file)
 
-	epub.create(file);
+	epub.create(file).then(function(folder) {
+
+		epub.fillData(folder, tempData)
+
+		// read from command line e template replace data
+		// add html files from markdown
+		// zip it
+
+	})
 }
