cf32e890372d923e324314e8c74a3c7da8a6857c
Carlo Panzi <carlo.panzi@gmail.com>
2016-08-02 12:20:52 +0200

    zip it

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const fs = require('fs-promise');
+const path = require('path');
 const options = require('commander');
 const marked = require('marked');
 
 const epub = require('./lib/EpubTemplate');
+const archiver = require('archiver');
 
 
 options
@@ -18,11 +20,20 @@ process.on('exit', () => {
 
 
 // Set to avoid duplicates
-let files = new Set(options.args.filter((file) => file.match(/.md$/i)));
+let files = new Set(options.args.filter((file) => file.match(/\.md$/i)));
+let ebookName = (file) => file.replace(/md$/i, 'epub');
+
+
+let d = new Date();
+let curr_date = d.getDate();
+let curr_month = d.getMonth() + 1; //Months are zero based
+let curr_year = d.getFullYear();
+let pubDate = curr_year + "-" + curr_month + "-" + curr_date;
 
 let tempData = {
 	title: 'Le avventure di carlo',
-	creator: 'Carlo'
+	creator: 'Carlo',
+	publication: pubDate
 }
 
 let readMD = function(file) {
@@ -41,9 +52,27 @@ for (let file of files) {
 		epub.create(file).then(function(folder) {
 
 			epub.fillData(folder, tempData)
+			return folder;
+			
 
+		}).then(function(folder) {
 			// zip it
+			let output = fs.createWriteStream( path.join(process.cwd(), ebookName(file)));
+			let archive = archiver('zip');
+			output.on('close', function() {
+				console.log(archive.pointer() + ' total bytes');
+				console.log('archiver has been finalized and the output file descriptor has closed.');
+			});
 
+			archive.on('error', function(err) {
+				throw err;
+			});
+
+			archive.pipe(output);
+			archive.bulk([
+			  { expand: true, cwd: folder, src: ['*'] }
+			]);
+			archive.finalize();
 		})
 
 	}).catch(function(err) {
