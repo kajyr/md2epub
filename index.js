50cf996abc0c16941b5c07849f1387ed6fb1da03
Carlo Panzi <carlo.panzi@gmail.com>
2016-08-03 07:38:17 +0200

    a bit of refactoring

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const options = require('commander');
 const marked = require('marked');
 
 const epub = require('./lib/EpubTemplate');
+const questions = require('./lib/interactive');
 const archiver = require('archiver');
 
 
@@ -22,19 +23,6 @@ process.on('exit', () => {
 // Set to avoid duplicates
 let files = new Set(options.args.filter((file) => file.match(/\.md$/i)));
 let ebookName = (file) => file.replace(/md$/i, 'epub');
-
-
-let d = new Date();
-let curr_date = d.getDate();
-let curr_month = d.getMonth() + 1; //Months are zero based
-let curr_year = d.getFullYear();
-let pubDate = curr_year + "-" + curr_month + "-" + curr_date;
-
-let tempData = {
-	title: 'Le avventure di carlo',
-	creator: 'Carlo',
-	publication: pubDate
-}
 
 let readMD = function(file) {
 	return fs.readFile(file, 'utf8')
@@ -58,29 +46,34 @@ let zipFolder = function(folder, zipFilePath) {
 	})
 }
 
+let gatherInformation = function(file) {
+	return readMD(file)
+	.then(function(mdData) {
+		return questions.ask().then((data) => {
+			data.html = marked(mdData)
+			return data;
+		})
+	});
+}
+
 for (let file of files) {
 
-	readMD(file).then(function(mdData) {
-
-		let html = marked(mdData);
-
-		tempData.html = html;
+	gatherInformation(file).then(function(data) {
 		// read from command line e template replace data
-
-		epub.create(file).then(function(folder) {
-
-			return epub.fillData(folder, tempData).then(() => folder)
-		
-		}).then(function(folder) {
-			// zip it
-			return zipFolder(folder, path.join(process.cwd(), ebookName(file)));
-		}).then(function(zipfile) {
-			console.log('Created', zipfile)
-		})
-
-	}).catch(function(err) {
+		return epub.create(file)
+		.then((folder) => epub.fillData(folder, data))
+	})
+	.then(function(folder) {
+		return zipFolder(folder, path.join(process.cwd(), ebookName(file)));
+	})
+	.then(function(zipfile) {
+		console.log('Created', zipfile)
+	})
+	.catch(function(err) {
 		if (err.code === 'ENOENT') {
 			console.log('Unable to open', err.path )
+		} else {
+			console.log(err)
 		}
 	})
 
